b18e2019567f2978d3ca5e19126524789dff87a4
tomastoews <tomas.toews@gmx.de>
2019-07-03 21:59:43 +0200

    Updated responsiveness

--- services/service.js
+++ services/service.js
@@ -13,17 +13,17 @@ spotifyApi.clientCredentialsGrant().then(
     }
 );
 
-spotifyApi.refreshAccessToken().then(
-    function(data) {
-      console.log('The access token has been refreshed!');
+// spotifyApi.refreshAccessToken().then(
+//     function(data) {
+//       console.log('The access token has been refreshed!');
   
-      // Save the access token so that it's used in future calls
-      spotifyApi.setAccessToken(data.body['access_token']);
-    },
-    function(err) {
-      console.log('Could not refresh access token', err);
-    }
-);
+//       // Save the access token so that it's used in future calls
+//       spotifyApi.setAccessToken(data.body['access_token']);
+//     },
+//     function(err) {
+//       console.log('Could not refresh access token', err);
+//     }
+// );
 
 function getSongs(query, limit) {
     return new Promise((resolve, reject) => {
